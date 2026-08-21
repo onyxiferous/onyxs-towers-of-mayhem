0f7a108525540153bf1d789b82eaf79c9b368185
onyxiferous <mycateclipse@gmail.com>
2026-08-21 14:56:19 -0500

    online description

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,10 +1,37 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { Client, GatewayIntentBits } = require('discord.js');
+const config = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
-
-client.once(Events.ClientReady, (readyClient) => {
-	console.log('Ready :3');
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
 });
 
-client.login(token);
+client.once('ready', async () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+
+    const onlineEmbed = new EmbedBuilder()
+	.setColor('#84ed80')
+	.setTitle('I\'m online!')
+	.setDescription('Hosted via Oracle Cloud on Ubuntu 24/7. This is just for testing.')
+	.setImage('https://media1.tenor.com/m/ckcRbRDEmN8AAAAC/smooth-brain-kitty.gif')
+	.setFooter({ text: 'Bot created by Onyx' });
+
+    const channelId = '1535497299252224110';
+    const channel = await client.channels.fetch(channelId).catch(console.error);
+
+    if (channel) {
+        channel.send({ embeds: [onlineEmbed] });
+    } else {
+        console.error('Could not find the channel. Make sure the bot is invited to that server!');
+    }
+});
+
+client.on('messageCreate', async (message) => {
+    // Ignore messages sent by bots
+    if (message.author.bot) return;
+});
+
+client.login(config.token);
