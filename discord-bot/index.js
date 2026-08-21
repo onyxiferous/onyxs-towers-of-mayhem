5ff9656fbb0119f2256b8ee5de7f8117c2f56eac
onyxiferous <mycateclipse@gmail.com>
2026-08-21 14:46:54 -0500

    modified:   discord-bot/.gitignore 	modified:   discord-bot/index.js

--- discord-bot/index.js
+++ discord-bot/index.js
@@ -1,1 +1,10 @@
-// placeholder
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, (readyClient) => {
+	console.log('Ready :3');
+});
+
+client.login(token);
