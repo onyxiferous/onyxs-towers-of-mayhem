96d29d1d522d583a9df7f6768b6e8a8506c073d5
onyx <mycateclipse@gmail.com>
2026-08-23 17:12:29 +0000

    modified:   main-website/server/api.js 	renamed:    main-website/api/data.json -> main-website/shared/data.json

--- main-website/server/api.js
+++ main-website/server/api.js
@@ -2,6 +2,6 @@ const path = require('node:path');
 
 app.get('/data.json', (req, res) => {
     res.sendFile(
-        path.join(__dirname, '../shared-data/data.json')
+        path.join(__dirname, '../shared/data.json')
     );
 });
